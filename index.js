9aee0f0fc04ab1a2915e25415c74f6cfce40f8ca
Esra Al <esra.wameed@yahoo.com>
2022-10-06 18:33:03 -0400

    added required package inquirer and created prompts for users

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
+//added requirements, fs already exists, npm installed inquire
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//use inquirer to create prompts in an array
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please insert your project title'
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+},
+{
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a project description',
+},
+{
+        type: 'input',
+        name: 'installation',
+        message: 'What are the necessary steps required to install project?',
+},
+{
+        type: 'input',
+        name: 'usage',
+        message: 'How is this project used?',
+},
+{
+        type: 'input',
+        name: 'contribution',
+        message: 'Guidelines on how others can contribute to this project?',
+},
+{
+        type: 'input',
+        name: 'testing',
+        message: 'How can this project be tested?',
+},
+{
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select a license for your project',
+        choices: ['Apache_2.0', 'MIT', 'MPL_2.0', 'None'],  
+},
+{
+        type: 'input',
+        name: 'githubUser',
+        message: 'Please enter your github user name',
+},
+{
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+},
+];
