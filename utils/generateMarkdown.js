7df993e0fe19c200e5333761c560accadf6f1010
Esra Al <esra.wameed@yahoo.com>
2022-10-06 18:41:43 -0400

    created a markdown generation function to be exported to index.js as a requirement, and added a readme for user input transferal

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,40 @@
+//creating generate Markdown function to be exported to index.js as module
+function generateMarkdown(data) {
+//crearing a dynamic license changing based on user input using literals
+  return `# ${data.title}
+
+  ## Licensing:
+  ![License: MIT](https://img.shields.io/badge/License-${data.licensing}-yellow.svg)
+
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.installation}
+
+  ## Usage:
+  ${data.usage}
+
+  ## License:
+  ${data.license}
+
+  ## Contribution:
+  ${data.contribution}
+
+  ## Testing:
+  ${data.testing}
+
+  ## Additional Info:
+  - Github: [${data.githubUser}](https://github.com/${data.githubUser})
+  - Email: ${data.email} `;
+}
+//to export package to index.js
+module.exports = generateMarkdown;
